6446eff2cabdcc245ae1bf1d8d5e282dbabd2f84
villalbaweb <villalbaweb@gmail.com>
2020-01-01 18:33:42 -0600

    Adding Navigation AI and NavMeshAgent componet and Mover.cs to player GO

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    // config options
+    [SerializeField] Transform target;
+
+    // cache
+    NavMeshAgent _agent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPos = new Vector3(target.position.x, target.position.y, target.position.z);
+        _agent.destination = targetPos;
+    }
+}
