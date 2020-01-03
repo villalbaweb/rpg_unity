616f34cf7be71e76e7f35bc1e2b4212bea52199b
villalbaweb <villalbaweb@gmail.com>
2020-01-02 21:09:42 -0600

    Adding Follow Camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    void Update()
+    {
+        transform.position = target.position;
+    }
+}
