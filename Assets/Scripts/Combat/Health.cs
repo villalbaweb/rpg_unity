0ef1bed2c123fd6efca792015fcdd652866cc1d2
villalbaweb <villalbaweb@gmail.com>
2020-01-11 17:53:23 -0600

    Triggering Die animation from Helath script, also fixin capsule collider to character base prefab

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -5,12 +5,28 @@ namespace RPG.Combat
     public class Health : MonoBehaviour 
     {
         // config params
-        [SerializeField] float health = 100f;
+        [SerializeField] float healthPoints = 100f;
+
+        // cache
+        Animator _animator;
+
+        // state
+        bool isDead;
+
+        private void Start() {
+            _animator = GetComponent<Animator>();
+
+            isDead = false;
+        }
 
         public void TakeDamage(float damage)
         {
-            health = Mathf.Max(health - damage, 0);
-            print(health);
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+
+            if (!isDead && healthPoints == 0) {
+                _animator.SetTrigger("Die");
+                isDead = true;
+            }
         }
     }
 }
