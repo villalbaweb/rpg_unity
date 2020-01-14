87a92f7ec3e160902771af67e07044ac4d722a4e
villalbaweb <villalbaweb@gmail.com>
2020-01-13 19:18:11 -0600

    Adding AIController sript and provide it with logic to identify wether the Player is within chaseRange

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        // config params
+        [SerializeField] float chaseDistance = 5f;
+
+        private void Update()
+        {
+            Chase();
+        }
+
+        private void Chase()
+        {
+            if (DistanceToPlayer() <= chaseDistance)
+            {
+                print($"{gameObject.name} in range to chase...");
+            }
+        }
+
+        private float DistanceToPlayer() {
+            GameObject player = GameObject.FindWithTag("Player");
+            return Vector3.Distance(player.transform.position, transform.position);
+        }
+    }
+}
