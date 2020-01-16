1e10bb2014a8986092e5b2b9772f61bfdf234fb7
villalbaweb <villalbaweb@gmail.com>
2020-01-16 11:54:42 -0600

    Adding PatrolPAth component and draw gizmos on select

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control 
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        const float gizmoSize = 0.3f;
+
+        private void OnDrawGizmosSelected() {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Vector3 gizmoPosition = transform.GetChild(i).position;
+
+                Gizmos.color = Color.red;
+                Gizmos.DrawSphere(gizmoPosition, gizmoSize);
+            }
+        }
+    }
+}
