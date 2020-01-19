c2f4efe528de69b993e9c37d40f39637d17052ab
villalbaweb <villalbaweb@gmail.com>
2020-01-18 18:32:00 -0600

    Adding patrolpaths to the enemies and also making the Player to attack even with mouse held down instead of have to click the mouse down

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -38,7 +38,7 @@ namespace RPG.Control
 
                 if(target == null || !_fighter.CanAttack(target.gameObject)) continue;
 
-                if(Input.GetMouseButtonDown(0))
+                if(Input.GetMouseButton(0))
                 {
                     _fighter.Attack(target.gameObject);
                 }
