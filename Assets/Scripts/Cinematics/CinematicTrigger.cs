47cbf454b5330cdfe47fe20e86e9ed6053a24bdb
villalbaweb <villalbaweb@gmail.com>
2020-01-29 22:19:08 -0600

    Adding box collider to Intro Sequence object in order to start the timeline

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        // state
+        bool IsCinematicTriggered = false;
+
+        private void OnTriggerEnter(Collider other) {
+
+            if(IsCinematicTriggered) return;
+
+            Debug.Log("colliding...");
+
+            GetComponent<PlayableDirector>().Play();
+            IsCinematicTriggered = true;
+        }
+    }
+}
+
