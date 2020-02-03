bc09a355cdf6e912f5b9ad36b39aaba458a4a1b1
villalbaweb <villalbaweb@gmail.com>
2020-02-02 22:13:00 -0600

    Making the Portal' script scene to load configurable

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -5,10 +5,13 @@ namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        // config params
+        [SerializeField] int sceneToLoad = -1;
+        
         private void OnTriggerEnter(Collider other) 
         {
             if (other.tag != "Player") return;
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(sceneToLoad);
         }
     }
 }
