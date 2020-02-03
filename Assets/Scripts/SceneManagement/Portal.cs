3089c12c94a84e7b414fc9b84a8f38492181b254
villalbaweb <villalbaweb@gmail.com>
2020-02-02 22:01:48 -0600

    Add Portal script to Portal object

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other) 
+        {
+            print("teletransport...");
+        }
+    }
+}
