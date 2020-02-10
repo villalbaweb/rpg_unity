8b233d1439687b1a391ca2bdf15ef6f2d8d634bf
villalbaweb <villalbaweb@gmail.com>
2020-02-09 18:29:34 -0600

    Creating persistent object

---
+++ Assets/Scripts/Core/PersistentObjectSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class PersistentObjectSpawner : MonoBehaviour
+    {
+        [SerializeField] GameObject persistentObjectPrefab;
+
+        static bool hasSpawned = false;
+
+        private void Awake() {
+            if(hasSpawned) return;
+
+            SpawnPersistentObject();
+            hasSpawned = true;
+        }
+
+        private void SpawnPersistentObject()
+        {
+            GameObject persistentObject = Instantiate(persistentObjectPrefab);
+            DontDestroyOnLoad(persistentObject);
+        }
+    }
+}
