3724e310881ae28f285f6be8533043eb8e42847e
villalbaweb <villalbaweb@gmail.com>
2020-02-12 00:20:03 -0600

    Adding the Saving Prefab to the Core nested prefab

---
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using RPG.Saving;
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string DEFAULT_SAVE_FILE = "save";
+
+        // cache
+        SavingSystem _savingSystem;
+
+        private void Start() 
+        {
+            _savingSystem = GetComponent<SavingSystem>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+
+            if(Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+        }
+
+        private void Save()
+        {
+            _savingSystem.Save(DEFAULT_SAVE_FILE);
+        }
+
+        private void Load()
+        {
+            _savingSystem.Load(DEFAULT_SAVE_FILE);
+        }
+    }
+}
