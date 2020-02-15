2bd12e83238f9185cdac13e08790cf9523d1f7da
villalbaweb <villalbaweb@gmail.com>
2020-02-15 00:19:42 -0600

    Now the Health data is Saveable as well, also added logix to restore after health has benn loaded

--- Assets/Scripts/Core/Health.cs
+++ Assets/Scripts/Core/Health.cs
@@ -1,8 +1,9 @@
+using RPG.Saving;
 using UnityEngine;
 
 namespace RPG.Core
 {
-    public class Health : MonoBehaviour 
+    public class Health : MonoBehaviour, ISaveable
     {
         // config params
         [SerializeField] float healthPoints = 100f;
@@ -36,6 +37,28 @@ namespace RPG.Core
                 IsDead = true;
                 _actionScheduler.CancelCurrentAction();
             }
+        }
+
+        public object CaptureState()
+        {
+            return healthPoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            healthPoints = (float)state;
+            UpdateAfterRestore();
+
+        }
+
+        private void UpdateAfterRestore()
+        {
+            IsDead = healthPoints == 0;
+            if(IsDead) {
+                _animator.SetTrigger("Die");
+            } else {
+                _animator.SetTrigger("AliveAfterReload");
+            } 
         }
     }
 }
