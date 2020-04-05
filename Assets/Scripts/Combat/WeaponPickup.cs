c877a7095f176e4ddda8d00f9cb696968b759a25
villalbaweb <villalbaweb@gmail.com>
2020-04-05 16:56:35 -0500

    Adding the Weapong Pick up mkechanism so we can pick up and equip weapons

---
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class WeaponPickup : MonoBehaviour
+    {
+        [SerializeField] Weapon weapon = null;
+
+        private void OnTriggerEnter(Collider other) 
+        {
+            if(other.tag != "Player") return;
+
+            Fighter _fighter = other.GetComponent<Fighter>();
+            _fighter.EquipWeapon(weapon); 
+
+            Destroy(gameObject);  
+        }
+    }
+}
