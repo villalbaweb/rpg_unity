1e8eca42b751c8247ea7f82af41a220f84953ca1
villalbaweb <villalbaweb@gmail.com>
2020-04-09 20:18:41 -0500

    Adding Simple Arrow asset, creatinf Arrow Projectile and add it the Projectile script that will follow a target on instantiation

---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Projectile : MonoBehaviour
+    {
+        // config params
+        [SerializeField] float speed = 10f;
+        [SerializeField] Transform target = null;
+
+        // Update is called once per frame
+        void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            if (!target) return;
+
+            transform.LookAt(target);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+}
