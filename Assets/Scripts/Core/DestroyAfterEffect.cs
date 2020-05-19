ce33603307eb403050f251366ae95e24fe182abd
villalbaweb <villalbaweb@gmail.com>
2020-05-18 23:43:09 -0500

    Adding a DestroyAfterEffect script so the impact particle effect gets destroyed as soon as its completed

---
+++ Assets/Scripts/Core/DestroyAfterEffect.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DestroyAfterEffect : MonoBehaviour
+{
+    void Update()
+    {
+        if(!GetComponent<ParticleSystem>().IsAlive())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
