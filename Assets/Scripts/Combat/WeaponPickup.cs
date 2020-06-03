a6275d22d94f6cc55819cdd0ce2bdcc3ac369a2a
villalbaweb <villalbaweb@gmail.com>
2020-06-02 21:06:57 -0500

    Instead of destroy the pickup on collision we will disable the collider and set inactive the actual visual, so we can reenable after a period of time

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace RPG.Combat
 {
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weapon = null;
+        [SerializeField] float respawnTime = 5.0f;
 
         private void OnTriggerEnter(Collider other) 
         {
@@ -13,7 +15,26 @@ namespace RPG.Combat
             Fighter _fighter = other.GetComponent<Fighter>();
             _fighter.EquipWeapon(weapon); 
 
-            Destroy(gameObject);  
+            StartCoroutine(HideForSeconds(respawnTime));
+        }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+
+            yield return new WaitForSeconds(seconds);
+
+            ShowPickup(true);
+        }
+
+        private void ShowPickup(bool show)
+        {
+            GetComponent<CapsuleCollider>().enabled = show;
+            foreach(Transform _child in transform)
+            {
+                _child.gameObject.SetActive(show);
+            }
+
         }
     }
 }
