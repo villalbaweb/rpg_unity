8740e7babf13d32b4ab9a550c934fc648e75beb8
villalbaweb <villalbaweb@gmail.com>
2020-06-07 16:10:12 -0500

    Adding BaseStats class to identify the type of object

---
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace RPG.Stats
+{
+    public class BaseStats : MonoBehaviour
+    {
+        [Range(1, 99)]
+        [SerializeField] int startingLevel = 1;
+        [SerializeField] CharacterClass characterClass;
+    }
+}
