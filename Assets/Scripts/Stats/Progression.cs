fbdb429fcedf3381da96df3a4d58e955dd1db91b
villalbaweb <villalbaweb@gmail.com>
2020-06-07 16:33:58 -0500

    Adding a Progression scriptable object so we can keep track of diff stats based on level

---
+++ Assets/Scripts/Stats/Progression.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace RPG.Stats
+{
+    [CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)]
+    public class Progression : ScriptableObject
+    {
+        [SerializeField] ProgressionCharacterClass[] characterClasses = null;
+
+        [System.Serializable]
+        public class ProgressionCharacterClass
+        {
+            [SerializeField] CharacterClass character;
+            [SerializeField] float[] health;
+        }
+    }
+}
