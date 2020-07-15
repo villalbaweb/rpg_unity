3d2b845e78ba29928b18ea2d53349b9f30d76903
villalbaweb <villalbaweb@gmail.com>
2020-07-15 16:53:08 -0500

    Adding XP Level data display

---
+++ Assets/Scripts/Stats/LevelDisplay.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Stats
+{
+    public class LevelDisplay : MonoBehaviour
+    {
+        // cache
+        BaseStats _playerBaseStats;
+        Text _text;
+
+        private void Awake()
+        {
+            _playerBaseStats = GameObject.FindWithTag("Player").GetComponent<BaseStats>();
+            _text = GetComponent<Text>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float experience = _playerBaseStats.GetExperienceLevel();
+            _text.text = experience > 0
+                ? string.Format("{0}", experience)
+                : "0";
+        }
+    }
+}
