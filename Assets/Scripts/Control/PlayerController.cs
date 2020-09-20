2b5639ee9ad3982d9b1d77ed90fcac470a150843
villalbaweb <villalbaweb@gmail.com>
2020-09-20 17:45:43 -0500

    Making the IRaycastable object to be sorted out by raycast hit distance so we can identify (cursor type) of the closets object)

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -57,7 +57,7 @@ namespace RPG.Control
 
         private bool InteractWithComponent()
         {
-            RaycastHit[] hitsData = Physics.RaycastAll(GetMouseRay());
+            RaycastHit[] hitsData = RaycastAllSorted();
 
             foreach (RaycastHit hit in hitsData)
             {
@@ -74,6 +74,22 @@ namespace RPG.Control
             }
 
             return false;
+        }
+
+        RaycastHit[] RaycastAllSorted()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+
+            float[] distances = new float[hits.Length];
+
+            for(int i = 0; i < hits.Length; i++)
+            {
+                distances[i] = hits[i].distance;
+            }
+
+            Array.Sort(distances, hits);
+
+            return hits;
         }
 
         private bool InteractWithMovement()
