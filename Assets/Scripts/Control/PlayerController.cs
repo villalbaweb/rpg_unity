bd9522bfaec4d1ff01a98422cee28ac5ee951d4d
villalbaweb <villalbaweb@gmail.com>
2020-09-21 22:20:21 -0500

    Added a configurable path distance and proper navmesh path that can be used

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -13,6 +13,7 @@ namespace RPG.Control
         // configs
         [SerializeField] CursorMapping[] cursorMappings = null;
         [SerializeField] float maxNavMeshProjectionDistance = 1f;
+        [SerializeField] float maxPathLenght = 10;
 
         // cache
         Mover _mover;
@@ -115,15 +116,41 @@ namespace RPG.Control
 
             RaycastHit hitData;
             bool hasHit = Physics.Raycast(GetMouseRay(), out hitData);
-            if(!hasHit) return false;
+            if (!hasHit) return false;
 
             NavMeshHit navMeshHit;
             bool hasCastToNavMesh = NavMesh.SamplePosition(hitData.point, out navMeshHit, maxNavMeshProjectionDistance, NavMesh.AllAreas);
-            
-            if(!hasCastToNavMesh) return false;
+
+            if (!hasCastToNavMesh) return false;
+
+            NavMeshPath path = new NavMeshPath();
+            bool hasPath = NavMesh.CalculatePath(transform.position, navMeshHit.position, NavMesh.AllAreas, path);
+
+            if (IsBadPath(path, hasPath)) return false;
 
             target = navMeshHit.position;
             return true;
+        }
+
+        private bool IsBadPath(NavMeshPath path, bool hasPath)
+        {
+            return !hasPath ||
+                    path.status != NavMeshPathStatus.PathComplete ||
+                    GetPathLength(path) > maxPathLenght;
+        }
+
+        private float GetPathLength(NavMeshPath path)
+        {
+            float result = 0;
+
+            if(path.corners.Length < 2) return result;
+
+            for(int i = 0; i < path.corners.Length - 1; i++)
+            {
+                result += Vector3.Distance(path.corners[i], path.corners[i + 1]);
+            }
+
+            return result;
         }
 
         private void SetCursor(CursorType cursorType)
