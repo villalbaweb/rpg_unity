131f31f61d28fda96d222f9e623212df57881937
villalbaweb <villalbaweb@gmail.com>
2020-09-23 23:33:05 -0500

    Now we destroy the DamageTExt clone object based on the UNity Event

---
+++ Assets/Scripts/UI/Destroyer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace PRG.UI.DamageText
+{
+    public class Destroyer : MonoBehaviour
+    {
+        [SerializeField] GameObject targetToDestroy = null;
+
+        public void DestroyTarget()
+        {
+            if(targetToDestroy)
+            {
+                Destroy(targetToDestroy);
+            }
+        }
+    }
+}
